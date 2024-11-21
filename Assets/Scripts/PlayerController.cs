463eaf8222fc15c6e00ec93980e65481d8f7698c
Rye L <148375551+ryeLS@users.noreply.github.com>
2024-11-21 16:28:07 -0500

    started task1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rb;
     public enum FacingDirection
     {
         left, right
@@ -12,7 +13,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
